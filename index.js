95ebdd6239e6a605730d4ae9f44325669c8b2243
emmanuel ronoh <eronoh036@gmail.com>
2024-07-17 13:15:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+    //Function explaining the scooter
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+  }
+     //Function explaing on driver
+  function Driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+     //Function explaining the pickupLocation
+  function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+  }
